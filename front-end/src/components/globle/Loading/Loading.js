34067267bb22a06071232aa00408219514adf307
tothanhquy <quyto111@gmail.com>
2021-09-13 08:21:09 +0700

    next

---
+++ front-end/src/components/globle/Loading/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './Loading.css';
+
+const Home = import('./routes/Home');
+
+function Loading() {
+	return(
+		<div className={style.loading}>
+			
+		</div>
+	);
+}
+export default Loading;
---
+++ front-end/src/components/globle/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './Logo.css';
+
+function Logo() {
+	return(
+		<div className={style.logo_component}>
+			<div className={}>
+				<div className={}>
+					<div className={}>
+
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+}
+export default Logo;
